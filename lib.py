2e7327355a610214372e1f75cf755760379a4113
Vũ Văn Học <2vhoc7@gmail.com>
2025-01-25 22:45:55 +0700

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,3 +1,7 @@
 # Libraries
-
-from sklean
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.svm import SVC
+from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
